2fd4ae0e061ae2dd0b4acb39e6c8fedf2421fbfb
Steven <Steven@DESKTOP-MO5PM5P>
2018-10-05 18:36:33 -0500

    test

---
+++ MethodArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodArray
+{
+	
+	class Program
+	{
+		public static bool IsEven(int value)
+		{
+			return value % 2 == 0;
+		}
+
+		static void Main(string[] args)
+		{
+			for (int i = 2; i <= 100; i++)
+			{
+				if (IsEven(i))
+				{
+					Console.WriteLine(i);
+					Console.ReadLine();
+				}
+			}
+		
+		}
+		
+	}
+}
